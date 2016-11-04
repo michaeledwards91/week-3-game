8292bd1e8b1186a1d56e8d43d7d074e26f55a7f6
michaeledwards91 <michael.edwards1991@gmail.com>
2016-11-03 20:11:08 -0500

    Game functionality complete(???). Needs styling and animation.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ arrays to hold possible words, letters guessed
 
 //counters
 var wins = 0;
-var words = ["goldfish", "guppy", "carp", "catfish", "suckermouth"];
+var words = ["goldfish", "guppy", "carp", "catfish", "suckermouth", "salmon", "koi", "northern pike", "gilt-head bream", "megalodon"];
 var wordIndex = 0; 
 var guessesRemaining = 10;
 //
@@ -36,11 +36,18 @@ function placeUnknownLetters() {
 		correctLetters.push(currentWord[i]);
 		//create a new list item to be added to the letters ul
 		var newli = document.createElement("li");
-		//give it a data attribute that contains its letter
-		newli.setAttribute("data-letter", currentWord.charAt(i));
-		newli.setAttribute("class", "listLetters");
-		//set its html to _ so that for now it displays as _
-		newli.innerHTML = "_";
+
+		if (alphabet.indexOf(currentWord[i]) >= 0) {
+			//give it a data attribute that contains its letter
+			newli.setAttribute("data-letter", currentWord.charAt(i));
+			newli.setAttribute("class", "listLetters");
+			//set its html to _ so that for now it displays as _
+			newli.innerHTML = "_";
+		} else {
+			newli.setAttribute("data-letter", "found");
+			newli.setAttribute("class", "found");
+			newli.innerHTML = currentWord[i];
+		}
 		//append it to letters ul
 		document.querySelector("#letters").appendChild(newli);
 		doneWithWord = false;
@@ -64,49 +71,45 @@ reveal a letter on hit, or add the incorrect letter to letters guessed section. 
 
 document.onkeyup = function(event) {
 	var userInput = String.fromCharCode(event.keyCode).toLowerCase(); //this var holds the key that user pressed
-	console.log(userInput);
-	//initialize game
 	console.log(correctLetters);
-	if (doneWithWord === true) {
-		placeUnknownLetters();
-	} else if (doneWithWord === false) {
-		//iterate through current word
-		var gotHit = false;
-		for (i = 0; i < currentWord.length; i++) {
-			//check if user input matches
-			if (userInput === currentWord[i]) {
-				//if it does, change the corresponding li html to the user input
-				document.querySelector("[data-letter="+currentWord[i]+"]").innerHTML = currentWord[i];
-				document.querySelector("[data-letter="+currentWord[i]+"]").setAttribute("class", "found");
-				//update gotHit
-				gotHit = true;
+	if (alphabet.indexOf(userInput) >= 0) {
+		if (doneWithWord === true) {
+			placeUnknownLetters();
+		} else if (doneWithWord === false) {
+			//iterate through current word
+			var gotHit = false;
+			for (i = 0; i < currentWord.length; i++) {
+				//check if user input matches
+				if (userInput === currentWord[i]) {
+					//if it does, change the corresponding li html to the user input
+					document.querySelector("[data-letter="+currentWord[i]+"]").innerHTML = currentWord[i];
+					document.querySelector("[data-letter="+currentWord[i]+"]").setAttribute("class", "found");
+					document.querySelector("[data-letter="+currentWord[i]+"]").setAttribute("data-letter", "found");
+					//update gotHit
+					gotHit = true;
+				}
 			}
+			//if user didnt get a hit and the guessed letter isnt already in letters guessed array
+			if (gotHit === false && lettersGuessed.indexOf(" " + userInput) < 0) {
+				guessesRemaining--;
+				lettersGuessed.push(" " + userInput);
+			}
+			//if guesses remaining is 0 
+			if (guessesRemaining === 0) {
+				alert("You lost");
+				wordIndex++;
+				resetGame();
+			} else if (document.querySelector(".listLetters") === null) {
+				wins++;
+				wordIndex++;
+				resetGame();
+			}
+			console.log("guesses remaining: " + guessesRemaining);
+			console.log("letters guessed: " + lettersGuessed);
+			console.log("word index is: " + wordIndex);
+			console.log("done with word: " + doneWithWord);
+			updateStats();
 		}
-		//if user didnt get a hit and the guessed letter isnt already in letters guessed array
-		if (gotHit === false && lettersGuessed.indexOf(userInput) < 0) {
-			guessesRemaining--;
-			lettersGuessed.push(" " + userInput);
-		}
-		//if guesses remaining is 0 
-		if (guessesRemaining === 0) {
-			alert("You lost");
-			wordIndex++;
-			resetGame();
-		} else if (document.querySelector(".listLetters") === null) {
-			wins++;
-			wordIndex++;
-			resetGame();
-		}
-		console.log("guesses remaining: " + guessesRemaining);
-		console.log("letters guessed: " + lettersGuessed);
-		console.log("word index is: " + wordIndex);
-		console.log("done with word: " + doneWithWord);
-		updateStats();
 	}
 	
-	//for loop, append an _ li in letters ul for each character that isnt a space in currentWord?
-
-	//use loop to iterate through characters of currentWord and push them into correctLetters array?
-	//test user input against correctLetters array, if match then change li to the found letter?
-
 }
